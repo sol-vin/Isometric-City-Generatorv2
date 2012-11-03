03ed37ebac1f3dac55ad929c61ac2065e006d347
Ian Rash <redcodefinal@gmail.com>
2012-11-03 02:42:50 -0700

    Commit 5
    Fix for last commit

---
+++ Isometric-City-Generatorv2/Isometric-City-Generatorv2/Isometric-City-Generatorv2/IsometricObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Isometric_City_Generatorv2
+{
+    public class IsometricObject
+    {
+        public Rectangle DrawRect;
+        public int Texture;
+        public Color Tint;
+        public bool Flip;
+
+        public virtual void Draw(SpriteBatch sb)
+        {
+
+        }
+
+    }
+}
